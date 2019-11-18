3bfebca5b1eb02627eef0170ca18d4fb956ee9ee
absolutechic <adaora.omcr@gmail.com>
2019-11-18 10:43:35 +0100

    first comit

---
+++ py_strings.py
@@ -0,0 +1,78 @@
+# name = input ("what is your name :")
+# # print(name)
+# # # print(name[0],name[2], name[4])
+# # # print(name[-1])
+# # # print(name[0:3])
+# # count = input("how long is this word  :")
+# # half = int(int(count)/2)-1
+# # rounded_num = round (half,0)
+# # two_chars_up = half + 2
+# # # print (name[half:])
+# # # print (name[0:2],name[half:half+2],name[-2:])
+# # first_two = name[0:2]
+# # mid_two = name[half:two_chars_up]
+# # last_two = name[-2:]
+# # statement = f"{first_two}{mid_two}{last_two}"
+# # print(statement)
+# # print (first_two,mid_two,last_two)
+# word = "sweet mun"
+# slice = word[::2]
+# slice2 = word[::]
+# slice3 = word[::-1]
+# slice4 = word[:5][::-1]
+# # print (slice)
+# # print (slice2)
+# # print (slice3)
+# print (slice4)
+# a = 10
+# b = 30
+# result = b%a
+# print (result)
+# sasha = 2**10
+# print (sasha)
+# d= 100//30
+# print (d)
+
+# x= 20
+# x = x+1
+# print (x)
+# x += 1
+# print (x)
+# x = 2016
+# x = 2016-4
+# print (x)
+# x -= 4
+# # print (x)
+# a = int(input("value of a"))
+# b = int(input ("value of b"))
+# c = int(input ("value of c"))
+# batch_a = ((b**2) - (4*a*c))**0.5
+# batch_b = (2*a)
+# print (batch_a)
+# print (batch_b)
+# # x1 = round(((-b+batch_a)/batch_b),2)
+# # x2 = round(((-b-batch_a)/batch_b),2)
+# x1 = ((-b+batch_a)/batch_b)
+# x2 = ((-b-batch_a)/batch_b)
+# # y = ((a**2)+(b**2))**0.5
+
+# print (x1, x2)
+
+# x = f"x is equal to {x1} or {x2}"
+# print (x)
+game_statement = f"Let us play a game to check if a word is a parlindrome. A Parlindrome is a word or phrase which when spelt backward gives the same word"
+print (game_statement)
+word = input("Type in any word of your choice")
+slice = word[::-1]
+print (slice)
+reverse = slice
+if (word == reverse):
+    if_statement = f"This is a parlindrome"
+    print (if_statement)
+else:
+    else_statement = f"This is not a parlindrome"
+    print (else_statement)
+vowels = ("a","e","i","o","u")
+any (vowel in word):
+any_statement = f""
+
